94afd54f7ed6bfec35f7bf4268fbe67fd143d4c8
undersourcecode <undersourcecode@hotmail.co.jp>
2018-11-13 07:27:55 +0900

    concurrencyを実装。

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 https://tutorialedge.net/course/golang/
 
 こちらのサイトのチュートリアルを写経、改変したもの。
+
+## concurrency
+
+https://tutorialedge.net/golang/concurrency-with-golang-goroutines/
+
+こちらのサイトのチュートリアルを写経、改変したもの。
---
+++ concurrency/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func compute(value int) {
+	for i := 0; i < value; i++ {
+		time.Sleep(time.Second)
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	fmt.Println("Goroutine Tutorial")
+
+	go compute(5)
+	go compute(5)
+
+	var input string
+	fmt.Scanln(&input)
+	fmt.Printf("your input = %s\n", input)
+}
